2f849523e9c7c5617c80c43ad717b07169794a48
jungeu1509 <jungeu1509@gmail.com>
2021-10-25 17:13:50 +0900

    upload project draft
    upload project draft

---
+++ src/main/java/com/progm/allsinsa/AllsinsaApplication.java
@@ -0,0 +1,13 @@
+package com.progm.allsinsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllsinsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllsinsaApplication.class, args);
+    }
+
+}
