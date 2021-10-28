47a8e706b565437404e67101146f8097aed36ec3
hyunjunkim <kdghj1002@gmail.com>
2021-10-28 13:46:28 +0900

    [AL-17, AL-21] feat. 제품 주문 및 조회 컨트롤러 *. 도메인 생성 테스트 추가

---
+++ src/main/java/com/progm/allsinsa/order/controller/OrderController.java
@@ -0,0 +1,51 @@
+package com.progm.allsinsa.order.controller;
+
+import com.progm.allsinsa.order.domain.Order;
+import com.progm.allsinsa.order.dto.CreateOrderRequestDto;
+import com.progm.allsinsa.order.dto.OrderDto;
+import com.progm.allsinsa.order.service.OrderService;
+import javassist.NotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> notFoundHandler(NotFoundException e) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createOrder(@RequestBody CreateOrderRequestDto createOrderRequestDto) {
+        String orderNumber = orderService.createOrder(createOrderRequestDto);
+        return ResponseEntity.ok(orderNumber);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Page<OrderDto>> getMemberOrder(@PathVariable("id") Long memberId, Pageable pageable) {
+        Page<OrderDto> memberOrderDto = orderService.getMemberOrder(pageable, memberId);
+        return ResponseEntity.ok(memberOrderDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<OrderDto>> getAllOrder(Pageable pageable) {
+        Page<OrderDto> allOrder = orderService.getAllOrder(pageable);
+        return ResponseEntity.ok(allOrder);
+    }
+
+    @GetMapping("/{number}")
+    public ResponseEntity<OrderDto> getOrder(@PathVariable("number") String orderNumber) throws NotFoundException {
+        OrderDto orderDto = orderService.getOrder(orderNumber);
+        return ResponseEntity.ok(orderDto);
+    }
+
+}
---
+++ src/test/java/com/progm/allsinsa/order/OrderControllerTest.java
@@ -0,0 +1,141 @@
+package com.progm.allsinsa.order;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.progm.allsinsa.order.dto.CreateOrderDto;
+import com.progm.allsinsa.order.dto.CreateOrderProductDto;
+import com.progm.allsinsa.order.dto.CreateOrderRequestDto;
+import com.progm.allsinsa.order.service.OrderService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureRestDocs
+@AutoConfigureMockMvc
+@SpringBootTest
+public class OrderControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    private String orderNumber1;
+    private String orderNumber2;
+    private Long memberId1;
+    private Long memberId2;
+
+    @BeforeEach
+    void setup() throws Exception {
+        memberId1 = 1L;
+        memberId2 = 2L;
+        CreateOrderDto createOrderDto1 = new CreateOrderDto(
+                memberId1,
+                "김현준1",
+                "01026846867",
+                "경기도 수원시 영통구 영통동",
+                "부재시 경비실에 맡겨주세요",
+                16000);
+        CreateOrderProductDto createOrderProductDto1 = new CreateOrderProductDto(
+                "바지",
+                3000,
+                2,
+                "사이즈 : S",
+                "https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.sportsseoul.com%2Fnews%2Fread%2F988300&psig=AOvVaw33h5sXl1K-OPcKgT7BmBGK&ust=1635391936561000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPj-tfLT6fMCFQAAAAAdAAAAABAD",
+                2L
+        );
+        CreateOrderProductDto createOrderProductDto2 = new CreateOrderProductDto(
+                "신발",
+                10000,
+                1,
+                "사이즈 : M",
+                "https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.sportsseoul.com%2Fnews%2Fread%2F988300&psig=AOvVaw33h5sXl1K-OPcKgT7BmBGK&ust=1635391936561000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPj-tfLT6fMCFQAAAAAdAAAAABAD",
+                3L
+        );
+        CreateOrderDto createOrderDto2 = new CreateOrderDto(
+                memberId2,
+                "김현준2",
+                "01026846867",
+                "경기도 수원시 영통구 영통동",
+                "부재시 경비실에 맡겨주세요",
+                16000);
+
+        CreateOrderProductDto createOrderProductDto3 = new CreateOrderProductDto(
+                "바지",
+                3000,
+                2,
+                "사이즈 : S",
+                "https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.sportsseoul.com%2Fnews%2Fread%2F988300&psig=AOvVaw33h5sXl1K-OPcKgT7BmBGK&ust=1635391936561000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPj-tfLT6fMCFQAAAAAdAAAAABAD",
+                2L
+        );
+        CreateOrderProductDto createOrderProductDto4 = new CreateOrderProductDto(
+                "신발",
+                10000,
+                1,
+                "사이즈 : M",
+                "https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.sportsseoul.com%2Fnews%2Fread%2F988300&psig=AOvVaw33h5sXl1K-OPcKgT7BmBGK&ust=1635391936561000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPj-tfLT6fMCFQAAAAAdAAAAABAD",
+                3L
+        );
+
+        CreateOrderRequestDto dto1 = new CreateOrderRequestDto(createOrderDto1, List.of(createOrderProductDto1, createOrderProductDto2));
+        CreateOrderRequestDto dto2 = new CreateOrderRequestDto(createOrderDto2, List.of(createOrderProductDto3, createOrderProductDto4));
+
+        orderNumber1 = orderService.createOrder(dto1);
+        orderNumber2 = orderService.createOrder(dto2);
+
+        mockMvc.perform(post("/orders")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto1)))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        mockMvc.perform(post("/orders")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto2)))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    public void findAllTest() throws Exception {
+        mockMvc.perform(get("/orders")
+                        .param("page", String.valueOf(0))
+                        .param("size", String.valueOf(2))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    public void findMemberOrderTest() throws Exception {
+        mockMvc.perform(get("/orders/{id}", memberId1)
+                .param("page", String.valueOf(0))
+                .param("size", String.valueOf(1))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        mockMvc.perform(get("/orders/{id}", memberId2)
+                        .param("page", String.valueOf(0))
+                        .param("size", String.valueOf(1))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+}
