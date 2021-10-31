0db387d77b99b68b0e8107826aa5d337d529c04d
jungeu1509 <jungeu1509@gmail.com>
2021-10-31 12:30:01 +0900

    [AL-18] refector. refector cartConverter
    remove unused converter

--- src/main/java/com/progm/allsinsa/cart/CartConverter.java
+++ src/main/java/com/progm/allsinsa/cart/CartConverter.java
@@ -11,39 +11,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class CartConverter {
 
-    public Cart convertCart(CartDto dto) {
-        Cart cart = new Cart(
-            dto.getId(),
-            dto.getMemberDto()
-        );
-        this.convertCartProducts(dto).forEach(cart::addCartProduct);
-        return cart;
-    }
-
-    public List<CartProduct> convertCartProducts(CartDto cartDto) {
-        return cartDto.getCartProductDtos().stream()
-            .map(cartProductDto -> {
-                return new CartProduct(
-                    cartProductDto.getId(),
-                    cartProductDto.getCount(),
-                    // TODO : ProductOption
-                    cartProductDto.getProductOptionDto()
-                    );
-            })
-            .collect(Collectors.toList());
-    }
-
-    public CartProduct convertCartProduct(Cart cart, CartProductDto cartProductDto) {
-        CartProduct cartProduct = new CartProduct(
-            cartProductDto.getId(),
-            cartProductDto.getCount(),
-            // TODO : ProductOption
-            cartProductDto.getProductOptionDto()
-        );
-        cartProduct.setCart(cart);
-        return cartProduct;
-    }
-
     public CartDto convertCartDto(Cart cart) {
         return CartDto.builder()
             .id(cart.getId())
